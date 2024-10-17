9c604b8e544b603ddd15e1ae6d2b931c42a05d81
UNKNOWTROLLERHASNOTBEENSEEN <115999637+UNKNOWTROLLERHASNOTBEENSEEN@users.noreply.github.com>
2024-10-17 18:33:05 +0400

    Add files via upload

---
+++ counting.py
@@ -0,0 +1,5 @@
+with open("file.txt") as file: # this will open the file that i want to be counted
+  lines = file.readlines() # this will read the lines and put them in a list
+  total_lines = len(lines) # it will check the length so basically lines
+  print("total:", total_lines) # it will basically print it
+#i hope you have a great day :3
